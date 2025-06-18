004ebef0963dae17f7c6b278ad111c84f76cd64c
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-06-18 16:31:06 +0530

    updated the link to focus to the url redierct

--- jackey.elixpo/JS/linkRedirect.js
+++ jackey.elixpo/JS/linkRedirect.js
@@ -1,27 +1,19 @@
 function getBasePath() {
-    // Determine the base path dynamically
-    if(window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost")
-    {
-      return "/jackey.elixpo"; 
-    }
-    else if (window.location.hostname === "circuit-overtime.github.io") {
-      return "/jackeyBot/jackey.elixpo"; 
-    }
-    else if (window.location.hostname.endsWith(".vercel.app")) 
-    {
-      return ""; 
-    }
-    else if (window.location.hostname.endsWith(".com"))
-    {
-      return "jackey.elixpo.com"
-    }  
-    else 
-    {
-      return "jackey.elixpo"; 
-    }
+  const hostname = window.location.hostname;
+
+  if (hostname === "127.0.0.1" || hostname === "localhost") {
+    return "/jackey.elixpo";
   }
-  
-  function redirectTo(path) {
-    const basePath = getBasePath();
-    location.replace(`${basePath}/${path}`);
+  else if (hostname === "circuit-overtime.github.io") {
+    return "/jackeyBot/jackey.elixpo";
   }
+  else if (hostname.endsWith(".vercel.app")) {
+    return "/jackey.elixpo";  
+  }
+  else if (hostname.endsWith(".com")) {
+    return "/jackey.elixpo"; 
+  }
+  else {
+    return "/jackey.elixpo";  
+  }
+}
