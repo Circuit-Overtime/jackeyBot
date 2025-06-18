c06407add391bd33db239ed707528d95433bff8a
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-06-18 16:47:13 +0530

    trial dir

--- jackey.elixpo/JS/linkRedirect.js
+++ jackey.elixpo/JS/linkRedirect.js
@@ -2,21 +2,16 @@ function getBasePath() {
   const hostname = window.location.hostname;
 
   if (hostname === "127.0.0.1" || hostname === "localhost") {
-    return "/jackey.elixpo";
+    return "/jackey.elixpo"; 
   }
   else if (hostname === "circuit-overtime.github.io") {
     return "/jackeyBot/jackey.elixpo";
   }
-  else if (hostname.endsWith(".vercel.app")) {
-    return "/jackey.elixpo";  // Vercel serves from this folder
-  }
-  else if (hostname.endsWith(".com")) {
-    return "/";  // Your .com also serves from this folder
-  }
   else {
-    return "/jackey.elixpo";  // Fallback
+    return ""; 
   }
 }
+
 
   
   function redirectTo(path) {
